89ff3f261f43491e305dc7eba40c974152222a36
Gabriel Grant <g@briel.ca>
2011-12-12 09:52:49 -0800

    add a middleware to render a view upon attempted database write

--- readonly/middleware.py
+++ readonly/middleware.py
@@ -2,7 +2,7 @@ from .exceptions import DatabaseWriteDenied
 from django.conf import settings
 from django.http import HttpResponse
 from django.utils.encoding import iri_to_uri
-
+from django.views.generic import TemplateView
 
 class HttpResponseReload(HttpResponse):
     """
@@ -35,3 +35,44 @@ class DatabaseReadOnlyMiddleware(object):
             # We can't do anything about this error
             return HttpResponse('The site is currently in read-only mode. '
                 'Please try again later.')
+
+class ReadOnlyTemplateView(TemplateView):
+    template_name = 'readonly/readonly.html'
+    def get_template_names(self):
+        template_names = []
+        if self.request.method == 'GET':
+            get_template = getattr(settings, 'DB_READ_ONLY_GET_TEMPLATE_NAME', None)
+            template_names.append(get_template)
+            template_names.append('readonly/get.html')
+        else:
+            post_template = getattr(settings, 'DB_READ_ONLY_POST_TEMPLATE_NAME', None)
+            template_names.append(post_template)
+            template_names.append('readonly/post.html')
+        default_template = getattr(settings, 'DB_READ_ONLY_TEMPLATE_NAME', None)
+        template_names.append(default_template)
+        template_names = [t for t in template_names if t]  # filter "None"s
+        template_names.extend(super(ReadOnlyTemplateView, self).get_template_names())
+        return template_names
+
+    # other handle methods the same as get
+    def head(self, request, *args, **kwargs):
+        return self.get(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.get(request, *args, **kwargs)
+
+    def options(self, request, *args, **kwargs):
+        return self.get(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.get(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.get(request, *args, **kwargs)
+
+class DatabaseReadOnlyViewMiddleware(object):
+    view_class = ReadOnlyTemplateView
+    def process_exception(self, request, exception):
+        if isinstance(exception, DatabaseWriteDenied):
+            # Only handle the correct exceptions
+            return self.view_class.as_view()(request)
